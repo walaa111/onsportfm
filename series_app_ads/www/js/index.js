0f646ae989a67ecfa7fab5763907520c72a3cd8a
walaa111 <walaagamil18@gmail.com>
2018-09-24 15:01:01 +0200

    add

--- series_app_ads/www/js/index.js
+++ series_app_ads/www/js/index.js
@@ -67,8 +67,8 @@ function initAd(){
                     interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
                 },
                 android : {
-                    banner: 'ca-app-pub-7251676025279948/4561656883',		//PUT ADMOB ADCODE HERE 
-                    interstitial: 'ca-app-pub-7251676025279948/5578977669'	//PUT ADMOB ADCODE HERE 
+                    banner: 'ca-app-pub-7251676025279948/9790964383',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-7251676025279948/1321964346'	//PUT ADMOB ADCODE HERE 
                 }
             };
             var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
